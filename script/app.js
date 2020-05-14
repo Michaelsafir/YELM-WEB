d66fc784a2d216f6e92e316fd8f6f2fcdc8164eb
Michael <yelm.io.media@gmail.com>
2020-05-15 00:50:24 +0300

    Publish WEB Service

---
+++ script/app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+
+new Chart(document.getElementById("chartjs-0"),{"type":"line","data":{"labels":["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресение"],"datasets":[{"label":"Посещения","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(33, 150, 243)","lineTension":0.1}, {"label":"Заказы","data":[13,16,13,18,23,14,8],"fill":false,"borderColor":"rgb(255, 149, 0)","lineTension":0.1}]},"options":{}});
+
+};
