68b587457ae5b7bc96dd83be3357b78618d6531b
Michael <yelm.io.media@gmail.com>
2020-05-15 22:31:40 +0300

    category loader

--- script/app.js
+++ script/app.js
@@ -1,5 +1,79 @@
 window.onload = function() {
 
-new Chart(document.getElementById("chartjs-0"),{"type":"line","data":{"labels":["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресение"],"datasets":[{"label":"Посещения","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(33, 150, 243)","lineTension":0.1}, {"label":"Заказы","data":[13,16,13,18,23,14,8],"fill":false,"borderColor":"rgb(255, 149, 0)","lineTension":0.1}]},"options":{}});
 
 };
+
+domain = "http://95.213.140.10/yelm/admin/api/api.php";
+platform = "yelm.io"
+categoty_change = ""
+
+function get_categories(platform) {
+    $.ajax({
+        type: "GET",
+        url: domain,
+        data: "get_categories=true&platform=" + platform,
+        success: function(msg) {
+            categories = msg
+            categories_block = '<div class="catalog_item"> <span class="catalog_subtitle">Название</span> <span class="catalog_subtitle">Товары</span> <span class="catalog_subtitle">Состояние</span> <span class="catalog_subtitle smart_actions_title">Действия</span> </div>'
+            console.log(categories)
+            for (var i = 0; i < categories.length; i++) {
+                category = categories[i].item
+                id = category.ID
+                count = categories[i].item_count
+                status = category.Status
+                color = ''
+                text = ''
+
+
+                switch (status) {
+                    case 'draft':
+                        color = 'orange'
+                        text = 'Черновик'
+                        break
+                    case 'rotation':
+                        color = 'green'
+                        text = 'В Ротации'
+                        break
+                    case 'stoped':
+                        color = 'red'
+                        text = 'Отключен'
+                        break
+                    default:
+                        break
+
+                }
+
+                categories_block += '<div class="catalog_item"> <span class="catalog_subtitle_inside">' + category.Name + '</span> <span class="catalog_subtitle_inside">' + count + ' шт.</span> <span class="catalog_subtitle_inside ' + color + '">' + text + '</span> <div class="smart_actions smart_actions_title"> <a href="#change-popup" onclick="change_category(`' + id + '`, `' + category.Name + '`, `' + status + '`)" class="open-popup-link blue"><i class="fas fa-edit"></i></a> <a href="#" class="red"><i class="fas fa-trash"></i></a> </div> </div>'
+
+            }
+
+            $(".catalog")[0].innerHTML = categories_block;
+            maginific()
+        }
+    });
+}
+
+
+
+function change_category(id, name, type) {
+    $(".change_category_name")[0].value = name
+
+
+
+    switch (type) {
+        case 'draft':
+            $(".change_category_type")[0].value = 3
+            break
+        case 'rotation':
+            $(".change_category_type")[0].value = 1
+            break
+        case 'stoped':
+            $(".change_category_type")[0].value = 2
+            break
+        default:
+            break
+
+    }
+
+    categoty_change = id
+}
